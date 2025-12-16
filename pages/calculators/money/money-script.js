2b143816764a2db6ab5bbf4d163a78eaad803ebd
RGO <rgokristof10@gmail.com>
2025-12-16 01:24:43 +0100

    fuh

--- pages/calculators/money/money-script.js
+++ pages/calculators/money/money-script.js
@@ -1,4 +1,36 @@
-let mem = [];
+let hintOpen = JSON.parse(localStorage.getItem("hintStatus")) || "open" ;
+if (hintOpen === "open") {
+    document.getElementById("hintBtn").innerHTML = "Segítség Bezár";
+    document.getElementById("header").innerHTML = 
+    "<h1>1 = 10ft , 2 = 20ft , 3 = 5ft</h1><h1>q = 100ft , w = 200ft , e = 50ft</h1><h1>r = reset , backspace = delete</h1>";
+    document.getElementById("hintBtn").className = 'nyitva';
+  }else{
+    document.getElementById("hintBtn").innerHTML = "Segítség Kinyit";
+    document.getElementById("hintBtn").className = 'zarva';
+  };
+function x() {
+  let hintOpen = JSON.parse(localStorage.getItem("hintStatus")) || "open" ;
+
+  if (hintOpen === "open") {
+    document.getElementById("header").innerHTML = "";
+    document.getElementById("hintBtn").innerHTML = "Segítség Kinyit";
+    document.getElementById("hintBtn").className = 'zarva';
+    hintOpen = "closed";
+  } else {
+    document.getElementById("header").innerHTML = 
+    "<h1>1 = 10ft , 2 = 20ft , 3 = 5ft</h1><h1>q = 100ft , w = 200ft , e = 50ft</h1>";
+    document.getElementById("hintBtn").innerHTML = "Segítség Bezár";
+    document.getElementById("hintBtn").className = 'nyitva';
+    hintOpen = "open";
+  }
+
+  localStorage.setItem("hintStatus",JSON.stringify(hintOpen))
+}
+
+let penz = JSON.parse(localStorage.getItem("penz")) || 0;
+document.querySelector('.js-osszeg').innerHTML =  penz+'ft' 
+
+let mem = JSON.parse(localStorage.getItem("mem")) || [];
 document.addEventListener('keydown', (event)=> {
     keyDown(event.key)
 });
@@ -19,11 +51,15 @@ function keyDown(key) {
     deleteLast()
   } else if (key === 'ji') {
     deleteLast()
+  } else if (key === 'r') {
+    reset()
   }
-
+  localStorage.setItem("mem",JSON.stringify(mem))
   console.log(mem.length)
+  mem = JSON.parse(localStorage.getItem("mem"))
   if (mem.length === 0) {
     document.querySelector('.js-osszeg').innerHTML = '0ft'
+    localStorage.setItem("penz",JSON.stringify(0))
   } else {
     let i = 0
     let osszeg = 0
@@ -33,6 +69,7 @@ function keyDown(key) {
     i++
   }
   document.querySelector('.js-osszeg').innerHTML = osszeg+'ft'
+  localStorage.setItem("penz",JSON.stringify(osszeg))
   }
   
   
@@ -41,6 +78,7 @@ function keyDown(key) {
 function reset() {
   mem = []
   document.querySelector('.js-osszeg').innerHTML = '0ft'
+  localStorage.setItem("penz",JSON.stringify(0))
 }
 function deleteLast() {
   mem.splice(mem.length-1,1)

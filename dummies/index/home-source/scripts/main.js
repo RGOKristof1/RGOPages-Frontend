e602f71e5ac8d17e9d85c0c81ff05306d2e6d4c0
RGO <rgokristof10@gmail.com>
2025-12-29 04:04:29 +0100

    yoyoyoyo

--- dummies/index/home-source/scripts/main.js
+++ dummies/index/home-source/scripts/main.js
@@ -1,3 +1,19 @@
-function alertonblur() {
-  alert("yo");
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-box");
+
+function doSearch() {
+  let szoveg = document.getElementById('search-box').value
+  alert(szoveg);
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  doSearch();
+});
+
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    form.requestSubmit();
+  }
+});

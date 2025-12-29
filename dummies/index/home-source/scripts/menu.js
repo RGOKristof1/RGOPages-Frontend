6345e6721670e1ee1575c4512e68b2205d8a7ab1
RGO <rgokristof10@gmail.com>
2025-12-29 20:35:22 +0100

    haha

--- dummies/index/home-source/scripts/menu.js
+++ dummies/index/home-source/scripts/menu.js
@@ -1,8 +1,17 @@
 
 
 function toggleMenu() {
-  let button = document.getElementById("other-button").classList.toggle("open");
-  console.log(document.getElementById("other-button").classList)
+  console.log(document.getElementById("other-button").classList.length)
+  if (document.getElementById("other-button").classList.length == 0) {
+    document.getElementById("menu-container").classList.remove('other-closed')
+    document.getElementById("menu-container").classList.add('other-open')
+    document.getElementById("other-button").classList.toggle("open")
+  } else {
+    document.getElementById("menu-container").classList.remove('other-open')
+    document.getElementById("menu-container").classList.add('other-closed')
+    document.getElementById("other-button").classList.toggle("open")
+  }
+  
   
   
 }

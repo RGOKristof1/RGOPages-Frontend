124373a2f5b12c68ebd04789322ea5ce646d9ea2
RGO <rgokristof10@gmail.com>
2026-01-12 03:09:33 +0100

    sadad

---
+++ dummies/bkvbufe/source/main.js
@@ -0,0 +1,34 @@
+
+let list = []
+console.log(list.length)
+function confirmAdd() {
+  let name = document.getElementById("name").value
+  let count = document.getElementById("count").value
+  if (name != "") {
+  list.push(`<li><p>${name} ${(count == 1) ? "" : `:${count}`}</p><button onclick="deleteThis(${(list.length == 1) ? "0" : `${list.length}`})">&#x2715;</button></li>`)
+  console.log(list.length)
+  }
+  refresh()
+}
+
+function refresh() {
+  let html = ""
+  for (let i = 0; i < list.length; i++) {
+    html += list[i]
+  }
+  document.getElementById("list").innerHTML = html
+}
+
+function deleteThis(index) {
+  console.log("doing some")
+  console.log(index)
+  list.splice(index, 1);
+  refresh()
+}
+
+function plusOne() {
+  document.getElementById("count").value = (Number(document.getElementById("count").value) + 1)
+}
+function minusOne() {
+  document.getElementById("count").value = (Number(document.getElementById("count").value) - 1)
+}

f3f714dc628d62abf9e62c569838d13e0f935e50
Rendszergazda <rendszergazda@blathy.info>
2026-01-12 10:25:12 +0100

    minden

--- dummies/bkvbufe/source/main.js
+++ dummies/bkvbufe/source/main.js
@@ -1,12 +1,15 @@
 
 let list = []
 function confirmAdd() {
+  hidePlusMinus()
   let name = document.getElementById("name").value
   let count = document.getElementById("count").value
   if (name != "") {
   list.push(`<li><p>${name} ${(count == 1) ? "" : `:${count}`}</p><button onclick="deleteThis(${list.length})">&#x2715;</button></li>`)
   console.log(list.length)
   }
+  document.getElementById("name").value = ""
+  document.getElementById("count").value = 1
   refresh()
 }
 
@@ -29,5 +32,20 @@ function plusOne() {
   document.getElementById("count").value = (Number(document.getElementById("count").value) + 1)
 }
 function minusOne() {
+  if (Number(document.getElementById("count").value) > 1)
   document.getElementById("count").value = (Number(document.getElementById("count").value) - 1)
+}
+
+function showPlusMinus() {
+  if (document.getElementById("name").value != "") {
+    document.getElementById("plus").classList.remove("hidden")
+    document.getElementById("minus").classList.remove("hidden")
+  } else {
+    document.getElementById("plus").classList.add("hidden")
+    document.getElementById("minus").classList.add("hidden")
+  }
+}
+function hidePlusMinus() {
+  document.getElementById("plus").classList.add("hidden")
+  document.getElementById("minus").classList.add("hidden")
 }

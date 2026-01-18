fbf40b6b253f7ee48f5091c217844ccda7271381
RGO <rgokristof10@gmail.com>
2026-01-18 19:09:03 +0100

    dsa

---
+++ CrosairyApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrosairyApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+

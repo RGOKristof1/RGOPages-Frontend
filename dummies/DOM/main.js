174c7e441476141ce1243db0e863d117cb5dbb14
Rendszergazda <rendszergazda@blathy.info>
2026-01-28 14:45:32 +0100

    sc

--- dummies/DOM/main.js
+++ dummies/DOM/main.js
@@ -1,4 +1,14 @@
-document.getElementById("item").innerText = JSON.parse(localStorage.getItem(inner)) || ""
-console.log(localStorage.getItem(inner))
-document.getElementById("item").innerText += "hi"
-localStorage.setItem(inner,JSON.stringify(document.getElementById("item").innerText))
+function refresh() {
+    switch (mode) {
+        case "light":
+            document.getElementById("style").setAttribute("href","style.css");
+            mode = "dark";
+            break;
+
+        case "dark":
+            document.getElementById("style").removeAttribute("href");
+            mode = "light";
+            break;
+    }
+}
+mode = "light"

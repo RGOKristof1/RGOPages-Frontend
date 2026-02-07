e604bb4e475c3cf7666e4df8e05e3c6f4e38573f
RGO <rgokristof10@gmail.com>
2026-02-07 04:59:44 +0100

    sa

--- rustyrackdemo/global-source/rustyDeerEyeGlow.js
+++ rustyrackdemo/global-source/rustyDeerEyeGlow.js
@@ -1,6 +1,6 @@
 document.getElementById("logo-img").addEventListener("mouseover", () => {
-  document.getElementById("logo-img").setAttribute("src","/rustyrackdemo/global-source/rustyDeerNoEye.png")
+  document.getElementById("logo-img").setAttribute("src","https://rgopages.hu/rustyrackdemo/global-source/rustyDeerWithEye.png")
 });
 document.getElementById("logo-img").addEventListener("mouseout", () => {
-  document.getElementById("logo-img").setAttribute("src","/rustyrackdemo/global-source/rustyDeerNoEye.png")
+  document.getElementById("logo-img").setAttribute("src","https://rgopages.hu/rustyrackdemo/global-source/rustyDeerNoEye.png")
 });

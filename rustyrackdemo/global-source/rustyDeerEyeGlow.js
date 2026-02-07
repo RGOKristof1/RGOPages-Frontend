2944808eb9653ce6c9aaca6a6b01af3e43a7c7e3
RGO <rgokristof10@gmail.com>
2026-02-07 03:12:11 +0100

    Refactor code structure for improved readability and maintainability

--- rustyrackdemo/global-source/rustyDeerEyeGlow.js
+++ rustyrackdemo/global-source/rustyDeerEyeGlow.js
@@ -1,6 +1,6 @@
-document.getElementById("logo").addEventListener("mouseover", () => {
-  document.getElementById("logo").setAttribute("src","global-source/rustyDeerWithEye.png")
+document.getElementById("logo-img").addEventListener("mouseover", () => {
+  document.getElementById("logo-img").setAttribute("src","global-source/rustyDeerWithEye.png")
 });
-document.getElementById("logo").addEventListener("mouseout", () => {
-  document.getElementById("logo").setAttribute("src","global-source/rustyDeerNoEye.png")
+document.getElementById("logo-img").addEventListener("mouseout", () => {
+  document.getElementById("logo-img").setAttribute("src","global-source/rustyDeerNoEye.png")
 });

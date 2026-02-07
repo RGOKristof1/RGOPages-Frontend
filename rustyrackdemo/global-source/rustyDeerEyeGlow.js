5a14666f265146c1c36867edc99fb8246878e4bd
RGO <rgokristof10@gmail.com>
2026-02-07 05:06:05 +0100

    ascacasc

--- rustyrackdemo/global-source/rustyDeerEyeGlow.js
+++ rustyrackdemo/global-source/rustyDeerEyeGlow.js
@@ -1,6 +1,8 @@
 document.getElementById("logo-img").addEventListener("mouseover", () => {
+  console.log("mousover")
   document.getElementById("logo-img").setAttribute("src","https://rgopages.hu/rustyrackdemo/global-source/rustyDeerWithEye.png")
 });
 document.getElementById("logo-img").addEventListener("mouseout", () => {
+  console.log("mousout")
   document.getElementById("logo-img").setAttribute("src","https://rgopages.hu/rustyrackdemo/global-source/rustyDeerNoEye.png")
 });

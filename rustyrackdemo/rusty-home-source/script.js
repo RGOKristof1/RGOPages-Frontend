abd97de116f0cdca5a196c5f326f6e4c15470cde
RGO <rgokristof10@gmail.com>
2026-02-09 04:03:48 +0100

    cleanup elvileg

--- rustyrackdemo/rusty-home-source/script.js
+++ rustyrackdemo/rusty-home-source/script.js
@@ -14,13 +14,13 @@ function toggleLanguage() {
 
 function refreshPageLanguage(){
   if (language == "hu") {
-  document.getElementById("lang-img").setAttribute("src","global-source/american-flag.webp")
+  document.getElementById("lang-img").setAttribute("src","/rustyrackdemo/rusty-global-source/images/american-flag.webp")
   document.getElementById("lang-text").innerText = "EN"
   document.getElementById("etlap-button").innerText = "Etlap"
   document.getElementById("rendelj-text").innerText = "Rendelj online"
   document.getElementById("bbq-text").innerText = "BBQ Szombat"
   document.getElementById("kostoldMeg-text").innerText = "Kóstold meg"
-  document.getElementById("rendelj-p").innerText = "Woltról rendelsz. Ugyanaz a friss íz érkezik."
+  document.getElementById("rendelj-p").innerText = "Woltról vagy Foodoraról rendelsz. Ugyanaz a friss íz érkezik."
   document.getElementById("bbq-p").innerText = "Szombaton az oldalas kerül a középpontba."
   document.getElementById("kostoldMeg-p").innerText = "Extra sajtos, pikáns cheddar jalapeño burger."
   document.getElementById("h2kosti").innerText = "Kóstold meg!"
@@ -28,13 +28,13 @@ function refreshPageLanguage(){
   document.getElementById("title").innerText = "Fooldal"
   console.log("set to hungarian")
   } else if (language == "en") {
-  document.getElementById("lang-img").setAttribute("src","global-source/hungarian-flag.webp")
+  document.getElementById("lang-img").setAttribute("src","/rustyrackdemo/rusty-global-source/images/hungarian-flag.webp")
   document.getElementById("lang-text").innerText = "HU"
   document.getElementById("etlap-button").innerText = "Menu"
   document.getElementById("rendelj-text").innerText = "Order for delivery"
   document.getElementById("bbq-text").innerText = "BBQ Saturday"
   document.getElementById("kostoldMeg-text").innerText = "Give it a try"
-  document.getElementById("rendelj-p").innerText = "Order from Wolt. Fresh taste arrives to you."
+  document.getElementById("rendelj-p").innerText = "Order from Wolt or Foodora. Fresh food always arrives to you fast."
   document.getElementById("bbq-p").innerText = "Saturday means ribs at Rusty Rack."
   document.getElementById("kostoldMeg-p").innerText = "Extra cheesy spicy cheddar jalapeno burger. Try it today."
   document.getElementById("h2kosti").innerText = "Try it!"
